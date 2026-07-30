56dd0c25e1edef9246ec12df6bcb0828dab24c96
Rolajraph <raphaellawrence078@gmail.com>
2026-07-30 23:53:59 +0100

    feat: add Category and Food controllers

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,37 @@
+import * as foodService from '../services/foodService.js';
+
+export const getFoods = async (req, res) => {
+  const foods = await foodService.getAllFoods(req.query);
+  res.status(200).json({ success: true, results: foods.length, data: { foods } });
+};
+
+export const getFood = async (req, res) => {
+  const food = await foodService.getFoodById(req.params.id);
+  res.status(200).json({ success: true, data: { food } });
+};
+
+export const createFood = async (req, res) => {
+  const food = await foodService.createFood(req.body);
+  res.status(201).json({
+    success: true,
+    message: 'Food item created successfully',
+    data: { food },
+  });
+};
+
+export const updateFood = async (req, res) => {
+  const food = await foodService.updateFood(req.params.id, req.body);
+  res.status(200).json({
+    success: true,
+    message: 'Food item updated successfully',
+    data: { food },
+  });
+};
+
+export const deleteFood = async (req, res) => {
+  await foodService.deleteFood(req.params.id);
+  res.status(200).json({
+    success: true,
+    message: 'Food item deleted successfully',
+  });
+};
